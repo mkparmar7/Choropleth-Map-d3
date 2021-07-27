7443fe725c4beba23d9004b6c33040c619f3d423
Mohan Kishor Parmar <mkparmar7@yahoo.com>
2021-07-27 15:18:10 +0530

    first commit Choropleth Map d3

---
+++ script.js
@@ -0,0 +1,95 @@
+// console.log(d3)
+// console.log(topojson)
+
+let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+let educationURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
+
+let countyData
+let educationData
+let county
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
+
+
+let drawMap = () => {
+
+    canvas.selectAll('path')
+    .data(countyData)
+    .enter()
+    .append('path')
+    .attr('d', d3.geoPath())
+    .attr('class', 'county')
+    .attr('fill', (countyData, item => {
+        let id = item['id']
+        let county = educationData.find((item) => {
+            return item['fips'] === id
+        })
+        let percentage = county['bachelorsOrHigher']
+        if(percentage <= 15){
+            return 'tomato'
+        } else if(percentage <= 30){
+            return 'orange'
+        } else if (percentage <= 45){
+            return 'lightgreen'
+        } else {
+            return 'limegreen'
+        }
+    }))
+    .attr('data-fips', (countyData, item => {
+        return item['id']
+    }))
+    .attr('data-education', (countyData, item => {
+        let id = item['id']
+        let county = educationData.find((item) => {
+            return item['fips'] === id
+        })
+        let percentage = county['bachelorsOrHigher']
+        return percentage
+    } 
+    ))
+
+    .on('mouseover', (countyData, countyDataItem) => {
+        
+        tooltip.transition()
+        .style('visibility', 'visible')
+
+        let id = countyDataItem['id']
+        let county = educationData.find((item) => {
+            return item['fips'] === id
+        })
+        tooltip.text(county['fips'] + '-' + county['area_name'] + ',' + county['state'] + ':' + county['bachelorsOrHigher'] + '%')
+        tooltip.attr('data-education', county['bachelorsOrHigher'])
+    
+        
+    })
+
+    
+    .on('mouseout', (countyData, item => {
+        tooltip.transition()
+        .style('visibility', 'hidden')
+    }))
+
+}
+
+d3.json(countyURL).then(
+    (data, error) => {
+    if(error){
+        console.log(error)
+    } else {
+        countyData = topojson.feature(data, data.objects.counties).features
+        console.log(countyData)
+
+        d3.json(educationURL).then(
+            (data, error) => {
+                if(error){
+                    console.log(error)
+                } else {
+                    educationData = data
+                    console.log(educationData)
+
+                    drawMap()
+                }
+            }
+        )
+    }
+})
